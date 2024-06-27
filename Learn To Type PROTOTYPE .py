d6ef6eaac1962538764e710d698ba4dd465a6324
bunnyfirth <bunnyfirth@outlook.com>
2024-06-27 17:59:47 -0400

    Initial commit

---
+++ Learn To Type PROTOTYPE .py
@@ -0,0 +1,9 @@
+print ('Today we are learning how to type.')
+print ('First, put your pointer fingers onto the bumps on the j and f keys.')
+print ('thoes are the keys you want your pointer fingers to always return to.')
+print ('Alright. so you know where the j and f keys are, but how about you press them in a patern.')
+text = input('Type jfjfjfjf: ')
+if text== ('jfjfjfjf'):
+    print ('Good job!')
+else:
+    print ('Too Bad. Maybe try again next time.')
